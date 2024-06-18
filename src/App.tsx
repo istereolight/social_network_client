fdc1d70ba77c0a042b20ed788971938179216c24
Vitaly Ishchenko <istereolight@gmail.com>
2024-06-18 16:18:20 +0300

    1.0.0

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from "@nextui-org/react";
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button color="primary">
+        123
+      </Button>
+    </div>
+  )
+}
+
+export default App
