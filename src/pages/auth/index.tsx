3e6b5349589e31d36eb889aa99bc977c4d95a707
Vitaly Ishchenko <istereolight@gmail.com>
2024-07-01 17:58:36 +0300

    2.1.2

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Tab, Tabs } from '@nextui-org/react';
 import React, { useState } from 'react'
+import { Login } from '../../features/login';
 
 export const Auth = () => {
   const [selected, setSelected] = useState('login');
@@ -15,7 +16,7 @@ export const Auth = () => {
               onSelectionChange={(key) => setSelected(key as string)}
             >
               <Tab key='login' title='Вход'>
-                Вход
+                <Login setSelected={setSelected} />
               </Tab>
               <Tab key='sign-up' title='Регистрация'>
                 Регистрация
