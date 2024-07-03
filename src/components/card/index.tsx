1d189566f564ce1e00100b56aceaa8fdf6bd9a2f
Vitaly Ishchenko <istereolight@gmail.com>
2024-07-03 16:11:55 +0300

    2.3.5

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react'
 
-import { CardHeader, Card as NextUiCard } from '@nextui-org/react'
+import { CardBody, CardFooter, CardHeader, Card as NextUiCard, Spinner } from '@nextui-org/react'
 import { useLikePostMutation, useUnlikePostMutation } from '../../app/services/likesApi';
 import { useDeletePostMutation, useLazyGetAllPostsQuery, useLazyGetPostByIdQuery } from '../../app/services/postsApi';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectCurrent } from '../../features/user/userSlice';
+import { User } from '../user';
+import { formatToClientDate } from '../../utils/format-to-client-date';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { Typography } from '../typography';
+import { MetaInfo } from '../meta-info';
+import { FcDislike } from 'react-icons/fc';
+import { MdOutlineFavoriteBorder } from 'react-icons/md';
+import { FaRegComment } from 'react-icons/fa';
+import { ErrorMessage } from '../error-message';
 
 type Props = {
   avatarUrl: string;
   name: string;
-  autorId: string;
+  authorId: string;
   commentId?: string;
+  content?: string;
   likesCount?: number;
   commentsCount?: number;
   createdAt?: Date;
@@ -23,11 +33,12 @@ type Props = {
 export const Card: React.FC<Props> = ({
   avatarUrl = '',
   name= '',
-  autorId = '',
+  authorId = '',
   commentId = '',
+  content = '',
   likesCount = 0,
   commentsCount = 0,
-  createdAt = Date,
+  createdAt,
   id = '',
   cardFor = 'post',
   likedByUser = false
@@ -46,9 +57,47 @@ export const Card: React.FC<Props> = ({
   return (
     <NextUiCard>
       <CardHeader className='justify-between items-center bg-transparent'>
-        <Link to={`/users/${autorId}`}>
+        <Link to={`/users/${authorId}`}>
+        <User 
+          name = {name}
+          className = 'text-small font-semibold leading-none text-default-600'
+          avatarUrl = {avatarUrl}
+          description = {createdAt && formatToClientDate(createdAt)}
+        />
         </Link>
+        {
+          authorId === currentUser?.id && (
+            <div className='cursor-pointer'>
+              {
+                deletePostStatus.isLoading || deleteCommentStatus.isLoading ? (
+                  <Spinner /> ) : (
+                  <RiDeleteBinLine />
+                )
+              }
+            </div>
+          )
+        }
       </CardHeader>
+      <CardBody className='px-3 py-2 mb-5'>
+        <Typography>{content}</Typography>
+      </CardBody>
+      {
+        cardFor !== 'comment' && (
+          <CardFooter className='gap-3'>
+            <div className='flex gap-5 items-center'>
+              <div >
+                <MetaInfo 
+                  count={likesCount} 
+                  Icon={likedByUser ? FcDislike : MdOutlineFavoriteBorder}/>
+              </div>
+              <Link to={`/posts/${id}`}>
+                <MetaInfo count={commentsCount} Icon={FaRegComment}/>
+              </Link>
+            </div>
+            <ErrorMessage error={error}/>
+          </CardFooter>
+        )
+      }
     </NextUiCard>
   )
 }
