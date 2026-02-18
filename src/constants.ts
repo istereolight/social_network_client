d73c46de76cd526d3fefac2545d59dedf0513583
System Administrator <root@iMac-Fixed-3.local>
2026-02-18 15:04:59 +0300

    18.02.26_posts adj

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://a36a0befb148.sn.mynetname.net"
-    : "http://localhost:3000"
+    ? "/api"
+    : "https://a36a0befb148.sn.mynetname.net/api"
 
 // export const BASE_URL = "http://localhost:3000"
 // export const BASE_URL = "http://a36a0befb148.sn.mynetname.net:3000"
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,42 +1,45 @@
-import React from 'react'
-import { useGetAllPostsQuery } from '../../app/services/postsApi'
-import { CreatePost } from '../../components/create-post';
-import { Card } from '../../components/card';
+import React from "react"
+import { useGetAllPostsQuery } from "../../app/services/postsApi"
+import { CreatePost } from "../../components/create-post"
+import { Card } from "../../components/card"
 
 export const Posts = () => {
-  const { data } = useGetAllPostsQuery();
+  const { data } = useGetAllPostsQuery()
   return (
     <>
-      <div className='mb-10 w-full'>
+      <div className="mb-10 w-full">
         <CreatePost />
       </div>
-      {
-        data && data.length > 0 
-          ? data.map(({
-            content,
-            author,
-            id,
-            authorId,
-            comments,
-            likes,
-            likedByUser,
-            createdAt
-          }) => (
-            <Card 
-              key={id}
-              avatarUrl={author.avatarUrl ?? ''}
-              content = {content}
-              name = {author.name ?? ''}
-              likesCount={likes.length}
-              commentsCount={comments.length}
-              authorId={authorId}
-              id={id}
-              likedByUser={likedByUser}
-              createdAt={createdAt}
-              cardFor='post'
-            />
-          )) : null
-      }
+      {data && data.length > 0
+        ? data.map(
+            ({
+              content,
+              author,
+              id,
+              authorId,
+              comments,
+              likes,
+              likedByUser,
+              createdAt,
+            }) => (
+              <div className="flex mb-2">
+                <Card
+                  key={id}
+                  avatarUrl={author.avatarUrl ?? ""}
+                  content={content}
+                  name={author.name ?? ""}
+                  likesCount={likes.length}
+                  commentsCount={comments.length}
+                  authorId={authorId}
+                  id={id}
+                  likedByUser={likedByUser}
+                  createdAt={createdAt}
+                  cardFor="post"
+                />
+              </div>
+            ),
+          )
+        : null}
     </>
   )
 }
